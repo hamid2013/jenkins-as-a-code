d66d5a67ce89315596f51f96dc4631a2ab6f9eef
hamid2013 <hraza@equalexperts.com>
2018-02-14 11:53:35 +0530

    adding second job

---
+++ job-dsl/secondpipeline.groovy
@@ -0,0 +1,25 @@
+def app_name = 'petclinic'
+/*
+    service/component build job
+*/
+pipelineJob("${app_name}-pipeline") {
+    concurrentBuild(false)
+    logRotator {
+        daysToKeep(14)
+    }
+    triggers {
+        scm('H/1 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github("hamid2013/spring-${app_id}", "https")
+                    }
+                    branch('refs/heads/master')
+                }
+            }
+            scriptPath("Jenkinsfile")
+        }
+    }
